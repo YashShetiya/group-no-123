a6c3a2559ca705f5daa5b35900a49cd3a1f18887
YashShetiya <yashxlency0410@gmail.com>
2021-04-17 23:07:49 +0530

    first commit

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserhomeComponent } from './userhome/userhome.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {UserService} from './user.service';
+import { HttpClientModule } from '@angular/common/http';
+import { VideoComponent } from './video/video.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserhomeComponent,
+    VideoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
